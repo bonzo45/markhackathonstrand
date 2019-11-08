e03a8267dc675a3adecb3475239762efe5b3336e
Sam Esgate <sam.esgate@sparx.co.uk>
2019-11-08 19:49:32 +0000

    Mexican wave...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import anime from 'animejs';
+import Thing from './Thing';
 
 function App() {
+  useEffect(() => {
+    anime({
+      targets: '.App-logo',
+      rotate: '1turn',
+      duration: 800
+    });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Things">
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+          <Thing />
+        </div>
       </header>
     </div>
   );
