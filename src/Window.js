aec700cb5b21b2963927f27d130ee0f804783d9b
Sam Esgate <sam.esgate@sparx.co.uk>
2019-11-30 15:06:08 +0000

    Put something in the window...

--- src/Window.js
+++ src/Window.js
@@ -7,7 +7,22 @@ function Window() {
         "height": "100%",
         "backgroundColor": "#e1f7ff",
         "color": "black",
+        "position": "relative",
     }}>
+        <div style={{
+            "position": "absolute",
+            "top": "0px",
+            "width": "100%",
+            "height": "50%",
+            "backgroundColor": "red",
+        }} />
+        <div style={{
+            "position": "absolute",
+            "top": "50%",
+            "width": "100%",
+            "height": "50%",
+            "backgroundColor": "blue",
+        }} />
     </div>
   );
 }
